53de7e004401c125917ecc65c946701558c5f9d7
vivek <vivekkumar16777@gmail.com>
2025-07-12 12:05:51 +0530

    added register and login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Login from './Component/Login.jsx'
 import App from './App.jsx'
+import Register from './Component/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Register/>
   </StrictMode>,
 )
