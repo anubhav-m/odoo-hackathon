c21dfa085987ca4fc7cb8f228505022693eac2c9
vivek <vivekkumar16777@gmail.com>
2025-07-12 11:01:12 +0530

    added style and modified searchbar

--- frontend/src/Component/Searchbar.jsx
+++ frontend/src/Component/Searchbar.jsx
@@ -2,25 +2,27 @@ import React from "react";
 
 function Searchbar() {
   return (
-    <div className="container mt-3">
-      <div className="d-flex align-items-center gap-3 w-100">
+    <div className="container-fluid mt-3"> 
+      <div className="d-flex flex-wrap align-items-center gap-3 w-100 justify-content-center">
 
-        <input
-          type="search"
-          className="form-control form-control-dark text-bg-dark"
-          placeholder="Ask New Question"
-          aria-label="Search"
-          style={{ maxWidth: "20%" }}
-        />
-        <div className="input-group" style={{ maxWidth: "40%" }}>
+       
+        <div className="flex-grow-1 d-flex justify-content-center" style={{ maxWidth: "30%" }}>
+          <button
+            className="btn btn-primary text-nowrap w-100"
+            style={{ fontSize: "16px" }}
+          >
+            Ask New Question
+          </button>
+        </div>
+        <div className="input-group flex-grow-1" style={{ maxWidth: "30%" }}>
           <input
             type="search"
             className="form-control form-control-dark text-bg-dark"
-            placeholder="Ask New Question"
+            placeholder="Newest Unanswered"
             aria-label="Search"
           />
           <button
-            className="btn btn-outline-light dropdown-toggle"
+            className="btn btn-outline-light dropdown-toggle text-nowrap"
             type="button"
             data-bs-toggle="dropdown"
             aria-expanded="false"
@@ -33,14 +35,17 @@ function Searchbar() {
             <li><a className="dropdown-item" href="#">Something else</a></li>
           </ul>
         </div>
-        <input
-          type="search"
-          className="form-control form-control-dark text-bg-dark"
-          placeholder="Ask New Question"
-          aria-label="Search"
-          style={{ maxWidth: "40%" }}
-        />
-        <button className="btn btn-primary">Search</button>
+        <div className="input-group flex-grow-1" style={{ maxWidth: "30%" }}>
+          <input
+            type="search"
+            className="form-control form-control-dark text-bg-dark"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-primary text-nowrap">
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
